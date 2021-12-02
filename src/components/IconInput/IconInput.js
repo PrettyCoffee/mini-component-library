78bc48b2034ab2bf1fbde3fa5eda53f3e57c14b4
PrettyCoffee <66521452+PrettyCoffee@users.noreply.github.com>
2021-12-02 16:31:45 +0100

    Implement IconInput

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -1,10 +1,56 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, {css} from 'styled-components';
 
 import { COLORS } from '../../constants';
 
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
+
+const getInputDimensions = (size) => {
+  const height = size === "small" ? "24px" : "36px";
+  const padding = size === "small" ? "4px" : "8px";
+  const fontSize = size === "small" ? "14px" : "18px";
+  const borderSize = size === "small" ? "1px" : "2px";
+
+  return css`
+    height: ${height};
+    padding-top: ${padding};
+    padding-bottom: ${padding};
+    padding-left: ${height};
+    font-size: ${fontSize};
+    box-shadow: inset 0 -${borderSize} 0 ${COLORS.black};
+  `;
+}
+
+const Wrapper = styled.div`
+  position: relative;
+  display: inline-flex;
+  align-items: center;
+  color: ${COLORS.gray700};
+  :hover {
+    color: black;
+  }
+`;
+
+const Input = styled.input`
+  ${({size, width}) => css`
+    width: ${width}px;
+    border: none;
+    color: inherit;
+    ${getInputDimensions(size)}
+    
+    ::placeholder{
+      color: ${COLORS.gray500}
+    }
+    :focus{
+      outline-offset: 2px;
+    }
+  `}
+`;
+
+const InputIcon = styled(Icon)`
+  position: absolute;
+`;
 
 const IconInput = ({
   label,
@@ -13,7 +59,11 @@ const IconInput = ({
   size,
   placeholder,
 }) => {
-  return 'TODO';
+  return <Wrapper>
+    <VisuallyHidden>{label}</VisuallyHidden>
+    <Input type="text" placeholder={placeholder} width={width} size={size}/>
+    <InputIcon id={icon} size={size === "small" ? 16 : 24}/>
+  </Wrapper>;
 };
 
 export default IconInput;
