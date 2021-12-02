f081424fec2b50611070e93dc206ed6ff516a357
PrettyCoffee <66521452+PrettyCoffee@users.noreply.github.com>
2021-12-02 16:32:48 +0100

    Implement Select

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -5,13 +5,55 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import { getDisplayedValue } from './Select.helpers';
 
+const Wrapper = styled.div`
+  position: relative;
+  display: inline-block;
+`;
+
+const NativeSelect = styled.select`
+  position: absolute;
+  inset: 0;
+  opacity: 0;
+  width: 100%;
+  cursor: pointer;
+`;
+
+const SelectFace = styled.div`
+  display: inline-flex;
+  align-items: center;
+  padding: 0 16px;
+  gap: 8px;
+  border-radius: 8px;
+  
+  color: ${COLORS.gray700};
+  background-color: ${COLORS.transparentGray15};
+  
+  ${NativeSelect}:hover + & {
+    color: ${COLORS.black};
+  } 
+  
+  ${NativeSelect}:focus-visible + & {
+    outline: solid 2px ${COLORS.gray700};
+  }
+`;
+
+const Label = styled.span`
+  padding: 12px 0;
+`;
+
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <NativeSelect value={value} onChange={onChange}>
+        {children}
+      </NativeSelect>
+      <SelectFace>
+        <Label>{displayedValue}</Label>
+        <Icon id='chevron-down' />
+      </SelectFace>
+    </Wrapper>
   );
 };
 
